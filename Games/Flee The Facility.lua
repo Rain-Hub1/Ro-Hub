246444515a3dec1a7f7c540230113a04593eec40
Rain-Hub1 <rainhubsla@gmail.com>
2025-11-28 12:11:03 -0300

    Update Flee The Facility.lua

--- Games/Flee The Facility.lua
+++ Games/Flee The Facility.lua
@@ -1,1 +1,10 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/jensonhirst/Orion/main/source')))()
 
+local Window = OrionLib:MakeWindow({
+    Name = "Ro Hub | " .. game.Name,
+    HidePremium = false,
+    SaveConfig = false,
+    ConfigFolder = "Ro Hub Config",
+    IntroEnabled = true
+    IntroText = "By rain / May contain bugs. / Pode conter bugs."
+})
