e47f436342a11a23e434674c68a0b6fcd95cf340
Rain-Hub1 <rainhubsla@gmail.com>
2025-11-28 11:38:04 -0300

    Update Games loading URL in Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,4 +1,4 @@
-local Games = loadstring(game:HttpGet(""))()
+local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/Rain-Hub1/Ro-Hub/refs/heads/main/List.lua"))()
 
 for PlaceID, Execute in pairs(Games) do
     if PlaceID == game.PlaceId then
