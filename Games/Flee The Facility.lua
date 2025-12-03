f365d08c70a44d3508b2ce4e3a59fa7e44b85365
Rain-Hub1 <rainhubsla@gmail.com>
2025-12-03 19:49:10 -0300

    Update Flee The Facility.lua

--- Games/Flee The Facility.lua
+++ Games/Flee The Facility.lua
@@ -1,39 +1,51 @@
-local OrionLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/jensonhirst/Orion/main/source"))()
-local Icons = loadstring(game:HttpGet("https://raw.githubusercontent.com/tlredz/Library/refs/heads/main/redz-V5-remake/Utils/Icons.lua"))()
+local Lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/Rain-Hub1/Ro-Lib/refs/heads/main/Source.lua"))()
 
-local Window = OrionLib:MakeWindow({
-    Name = "Ro Hub | Flee The Facility",
-    HidePremium = false,
-    SaveConfig = false,
-    ConfigFolder = "Ro Hub Config",
-    IntroEnabled = true,
-    IntroText = "By rain / May contain bugs. / Pode conter bugs."
+local Win = Lib:Window({
+  Title = "My window!"
 })
 
-local InfoTab = Window:MakeTab({
-	Name = "Info",
-	Icon = Icons.user,
-	PremiumOnly = false
+local MinWin = Win:MinimizeWin({
+  Format = "Circle"
 })
 
-local InfoSLP = InfoTab:AddSection({
-	Name = "Info | LocalPlayer"
+local EspTab = Win:Tab({
+  Name = "Esp"
 })
 
-local Players = game:GetService("Players")
-local LocalPlayer = Players.LocalPlayer
+function new(c, p)
+  local k = Instance.new(c)
+    for pp, v in pairs(p or {}) do
+      k[pp] = v
+    end
+  return k
+end
 
-local LInfoLPN = InfoTab:AddLabel("Hello " .. LocalPlayer.Name .. "!")
+local plrs = game:GetService("Players")
+local lp = plrs.LocalPlayer
+local MyCter = lp.Character
 
-local SettingsTab = Window:MakeTab({
-	Name = "Settings",
-	Icon = Icons.settings,
-	PremiumOnly = false
-})
-
-OrionLib:MakeNotification({
-	Name = "Ro Hub | Info",
-	Content = "Hub loaded 100%",
-	Image = "rbxassetid://4483345998",
-	Time = 5
+local BeastEsp = EspTab:Toggle({
+  Name = "Beast",
+  Desc = "highlights the beast",
+  Default = false,
+  Callback = function()
+    for 0, 1 do 
+      local hl
+      if v == true then
+        for v, p in pairs(plrs:GetPlayers()) do
+          if p and p.Character and p.Hammer then
+            hl = new("Highlight", {
+              Adornee = p,
+              FillColor = Color.fromHex("#00ff33")
+              FillTransparency = 0.5,
+              Parent = p
+            })
+          end
+        end
+      elseif v == false then
+        task.wait(0.1)
+        hl:Destry()
+      end
+    end
+  end
 })
