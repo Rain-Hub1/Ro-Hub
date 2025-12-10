5789c1355f98e3aae84fe7c6701cfe0fea47eb4e
Rain-Hub1 <rainhubsla@gmail.com>
2025-12-10 01:26:44 -0300

    Update List.lua

--- List.lua
+++ List.lua
@@ -1,5 +1,6 @@
 local Games = {
-  [893973440] = "https://raw.githubusercontent.com/Rain-Hub1/Ro-Hub/refs/heads/main/Games/Flee%20The%20Facility.lua" -- Flee The Facility
+  [893973440] = "https://raw.githubusercontent.com/Rain-Hub1/Ro-Hub/refs/heads/main/Games/Flee%20The%20Facility.lua", -- Flee The Facility
+  [126331002216562] = "https://raw.githubusercontent.com/Rain-Hub1/Ro-Hub/refs/heads/main/Games/Chasing.lua" -- [UPD] Chasing 🔪
 }
 
 return Games
